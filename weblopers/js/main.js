40e1e72be34329776c984a2e4e3fde4ec1bb4d6a
Anknit <Anknit@75440f0c-d9f7-4522-9995-a6677e94c25c>
2016-04-05 03:31:14 +0000

    medical

--- weblopers/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    if(typeof(quickTopic) != "undefined"){
-        $('[data-link="'+quickTopic+'"]').addClass('active');
-    }
-    else{
-        $('[data-link]').eq(0).addClass('active');
-    }
-})
